1b44f33f3a05985e406388b386fd3263f577df3c
yash-bissa <yashkumar100404@gmail.com>
2025-12-19 20:01:46 +0530

    added cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const authRoutes = require('../src/routes/auth.route');
 const cookieParser = require("cookie-parser")
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
 
 app.use(express.json());
 app.use(cookieParser());
